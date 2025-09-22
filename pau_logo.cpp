882334f6142d9f2002cb2788d0114c122e834487
PAULOOOOHHH <paul.matthew.vlog@gmail.com>
2025-09-22 12:11:56 +0800

    Create pau_logo.cpp

---
+++ pau_logo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+cout << "======================\n";
+cout << " PAU LOGO \n";
+cout << "======================\n";
+cout << "PPPP AAAAA U U\n";
+cout << "P P A A U U\n";
+cout << "PPPP AAAAAAA U U\n";
+cout << "P A A U U\n";
+cout << "P A A UUU \n";
+cout << "======================\n";
+cout << "Made with std::cout :)\n";
+return 0;
+}
