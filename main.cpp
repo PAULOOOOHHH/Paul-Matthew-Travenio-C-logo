b9ab48368a44c1f142c18a1987cb929251e38296
PAULOOOOHHH <paul.matthew.vlog@gmail.com>
2025-09-22 12:25:23 +0800

    Delete main.cpp

--- main.cpp
+++
@@ -1,7 +0,0 @@
-git init
-git add .
-git commit -m "Initial commit: add C++ ASCII logo"
-# replace with your repo's URL created on GitHub:
-git remote add origin <YOUR-GITHUB-REPO-URL>
-git branch -M main
-git push -u origin main
